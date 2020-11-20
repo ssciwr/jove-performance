a61981b6a7faa0e4fd5b111b278313711dfaff53
Benjamin Thomitzni <benjamin.thomitzni@iwr.uni-heidelberg.de>
2020-11-20 16:06:33 +0100

    added h3coh data and variable test call

--- tests/libjove_tests.C
+++ tests/libjove_tests.C
@@ -7,7 +7,8 @@ using namespace std;
 
 int main (){
         do_mp2_test mp2_test;
-        cout << "Performing tests for libjove"<<endl;
-        cout << "Testing do_mp2 ... "<<((mp2_test.run_test()) ? "passed" : "failed")<<endl;
-        cout << "Tests done. Exiting."<<endl;
+        cout << "Performing tests for libjove" << endl;
+        cout << "Testing do_mp2 (H2O case) ... " << flush << ((mp2_test.run_test("../data/h2o_mp2/", -0.12422, 0.0001)) ? "passed" : "failed") << endl;
+        //cout << "Testing do_mp2 (H3COH case) ... " << flush << ((mp2_test.run_test("../data/h3coh_mp2/", -0.21635, 0.0001)) ? "passed" : "failed") << endl;
+        cout << "Tests done. Exiting." << endl;
 }
