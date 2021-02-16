4651529a35ea899924f9fe2e9d270b1d565391aa
Liam Keegan <liam@keegan.ch>
2021-02-16 21:15:02 +0100

    add c14h10 test case (not including data)

--- tests/libjove_tests.C
+++ tests/libjove_tests.C
@@ -9,6 +9,7 @@ int main (){
         do_mp2_test mp2_test;
         cout << "Performing tests for libjove" << endl;
         // cout << "Testing do_mp2 (H2O case) ... " << flush << ((mp2_test.run_test("../data/h2o_mp2/", -0.12422, 0.0001)) ? "passed" : "failed") << endl;
-        cout << "Testing do_mp2 (H3COH case) ... " << flush << ((mp2_test.run_test("../data/h3coh_mp2/", -0.21635, 0.0001)) ? "passed" : "failed") << endl;
+        // cout << "Testing do_mp2 (H3COH case) ... " << flush << ((mp2_test.run_test("../data/h3coh_mp2/", -0.21635, 0.0001)) ? "passed" : "failed") << endl;
+        cout << "Testing do_mp2 (C14H10 case) ... " << flush << ((mp2_test.run_test("../data/c14h10_mp2/", -1.24223, 0.0001)) ? "passed" : "failed") << endl;
         cout << "Tests done. Exiting." << endl;
 }
